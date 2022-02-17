4c9fc9edfa47916ba1c300d9aa3962fde9a247c8
Maksymilian Roman <maksymilianroman1@gmail.com>
2022-02-17 14:20:43 +0100

    Usunięcie komentarzy w Python i Python2

--- Python2.py
+++ Python2.py
@@ -4,11 +4,8 @@ sql_code = file.read()
  
 i = 1950
 while i<=2022:
-	#napis = '4,"renault","Renault","French","http://en.wikipedia.org/wiki/Renault_in_Formula_One"\n'
     name = "Okrazenia-" + str(i) + ".sql"
-    # print(name)
-    #file2 = open(f"ala{i}.sql", "w")  # ala1.sql  ala2.sql
-    file2 = open(name, "w")  # ala1.sql  ala2.sql
+    file2 = open(name, "w")
     t = sql_code.replace("<YEAR>", str(i))
     i += 1
     file2.write(t)
